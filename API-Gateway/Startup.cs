cb94c003f218c4e17ab7fe920862a87895a7b2cf
Wesley Haagmans <wesleytuuh@gmail.com>
2021-06-14 04:25:01 +0200

    ~UPDATE: Ocelot to redirect to cluster services
    ~UPDATE: F
    
    
    ~UPDATE: Ocelot update rename
    
    
    up
    
    
    ~FIX: Ocelot

--- API-Gateway/Startup.cs
+++ API-Gateway/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Kubernetes;
 
 namespace API_Gateway
 {
@@ -22,7 +23,7 @@ namespace API_Gateway
 		{
 			services.AddCors();
 
-			services.AddOcelot();
+			services.AddOcelot().AddKubernetes();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
